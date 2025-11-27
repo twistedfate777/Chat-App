2d2408a720e5009c9f5c345f51508994a5133f34
Pascal <mainbuat575@gmail.com>
2025-11-27 10:39:24 +0700

    fixed cookie maxAge

--- backend/src/utils/generateTokenAndSetCookies.js
+++ backend/src/utils/generateTokenAndSetCookies.js
@@ -4,10 +4,10 @@ export const generateTokenAndSetCookies =  (userId,res)=>{
   const token = jwt.sign({userId:userId},process.env.JWT_SECRET,{
     expiresIn:"7d"
   })
-
+  console.log(process.env.NODE_ENV)
   res.cookie("jwt",token,{
     maxAge: 7 * 24 * 3600 * 1000,
     httpOnly:true,
-    sameSite:"strict",
+    sameSite:"none",
     secure : process.env.NODE_ENV === "production"
   })}
