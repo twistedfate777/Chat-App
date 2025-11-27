f95323f8b6fa5eebd8a0ea42f3a87e61e7d55b5e
Pascal <mainbuat575@gmail.com>
2025-11-27 10:19:45 +0700

    fixed trailing slash error

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -14,9 +14,9 @@ const PORT = process.env.PORT || 5000
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://chat-app-l5cz.vercel.app/", 
-  "https://chat-app-l5cz-2koybxj9e-twistedfate777s-projects.vercel.app/",
-  "https://chat-app-l5cz-26ly7cmfc-twistedfate777s-projects.vercel.app/"
+  "https://chat-app-l5cz.vercel.app", 
+  "https://chat-app-l5cz-2koybxj9e-twistedfate777s-projects.vercel.app",
+  "https://chat-app-l5cz-26ly7cmfc-twistedfate777s-projects.vercel.app"
 ];
 
 app.use(cors({
